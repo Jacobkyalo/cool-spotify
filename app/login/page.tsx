6b12ea6320566e1cc56f190da298c5f1ad3662f5
Jacob Kyalo <jacobkyalo43@gmail.com>
2024-05-21 12:26:33 +0300

    add login & signup routes

---
+++ app/login/page.tsx
@@ -0,0 +1,15 @@
+import LoginForm from "@/components/login-form";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Login",
+  description: "Login to your account on Cool Spotify",
+};
+
+export default function Login() {
+  return (
+    <main>
+      <LoginForm />
+    </main>
+  );
+}
---
+++ app/signup/page.tsx
@@ -0,0 +1,15 @@
+import SignupForm from "@/components/signup-form";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Sign Up",
+  description: "Create an account on Cool Spotify",
+};
+
+export default function Signup() {
+  return (
+    <main>
+      <SignupForm />
+    </main>
+  );
+}
