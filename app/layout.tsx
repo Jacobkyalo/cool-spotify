1c7c5fcb15ed2509b1401dab9a21f3130341e8bb
Jacob Kyalo <jacobkyalo43@gmail.com>
2024-05-21 12:27:27 +0300

    add providers for auth and toast context

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import AuthProvider from "@/lib/auth";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +19,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <AuthProvider>{children}</AuthProvider>
+        <ToastContainer />
+      </body>
     </html>
   );
 }
