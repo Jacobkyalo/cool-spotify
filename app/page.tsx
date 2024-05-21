eaa16603ceec6af05396b0b7b82b111499bedeb4
Jacob Kyalo <jacobkyalo43@gmail.com>
2024-05-21 12:28:19 +0300

    protect / route

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,20 @@
+"use client";
+
 import Navbar from "@/components/navbar";
 import SearchForm from "@/components/search-form";
+import { useAuth } from "@/lib/auth";
+import { redirect } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
+  const { user } = useAuth();
+
+  useEffect(() => {
+    if (!user) {
+      redirect("/login");
+    }
+  }, [user]);
+
   return (
     <div className="container">
       <Navbar />
